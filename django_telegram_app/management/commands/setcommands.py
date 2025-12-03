fd7380bd6d46be815afbc5606c3b379d4fb8a6b4
Sonny V <sonny@softllama.net>
2025-12-03 08:25:23 +0100

    style: remove unused import

--- django_telegram_app/management/commands/setcommands.py
+++ django_telegram_app/management/commands/setcommands.py
@@ -1,7 +1,6 @@
 """Django command to set telegram commands."""
 
 import requests
-from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 from django.utils.translation import override
 
